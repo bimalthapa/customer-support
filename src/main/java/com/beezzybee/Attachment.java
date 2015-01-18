59c17a42cf439d8a2f4fb05dddfb2ce4fed93222
Bimal Thapa <bthapa@Bimals-Mac-mini.local>
2015-01-18 11:25:33 -0500

    Complete Servlet Implementation

---
+++ src/main/java/com/beezzybee/Attachment.java
@@ -0,0 +1,24 @@
+package com.beezzybee;
+
+public class Attachment {
+
+	private String name;
+	private byte[] contents;
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public byte[] getContents() {
+		return contents;
+	}
+
+	public void setContents(byte[] contents) {
+		this.contents = contents;
+	}
+
+}
